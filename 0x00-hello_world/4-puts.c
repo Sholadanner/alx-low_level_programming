3a4ba4a4201334a93da424db065dfb462156ee78
Sholadanner <judedanner@gmail.com>
2023-09-07 14:49:37 -0700

    Printing a string in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
+ * main - the main funtion is given below
+ *
  * This is a c program that demonstrates how to print a string.
  *
  * The command used was puts insted of printf
- *
- * main - the main function, which is the entry point of the code is given below
  */
 
 int main(void)
