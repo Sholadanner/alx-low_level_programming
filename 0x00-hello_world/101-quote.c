6105d29468dbc08beca35f8185c028a10b724718
Sholadanner <judedanner@gmail.com>
2023-09-07 19:36:11 -0700

    Printing strings to the standand error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdin.h>
+#include <unistd.h>
+/**
+ * main - this program shows the function with int return type
+ *
+ * Return: the return has an argument of 1
+ *
+ * This program intends to print a string
+ */
+int main(void)
+{
+	putc("and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
