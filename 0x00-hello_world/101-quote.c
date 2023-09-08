8b490f520fab301c56686546d20f70d4e63fd5b6
Sholadanner <judedanner@gmail.com>
2023-09-07 19:45:26 -0700

    Printing a string to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdin.h>
 #include <unistd.h>
 /**
  * main - this program shows the function with int return type
@@ -9,6 +8,6 @@
  */
 int main(void)
 {
-	putc("and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
