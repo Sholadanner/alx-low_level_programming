09202748baf3f2b6314e98aee11dd38b985370cc
Sholadanner <judedanner@gmail.com>
2023-09-08 16:34:53 -0700

    Displaying if a random numer is +v, -v or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,11 +21,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n > 0) {
-		printf("%ln is positive\n");
+		printf("%d is positive\n", n);
 	} else if(n == 0) {
-		printf("%ln is zero\n");
+		printf("%d is zero\n", n);
 	} else {
-		printf("%ln is negative\n");
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
