29847b177020d08897445a708832b8e5a3595b26
Sholadanner <judedanner@gmail.com>
2023-09-07 17:24:37 -0700

    Printing a string with printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program shows the main function with int return type
+ *
+ * Return: the return has no arguments, 0
+ *
+ * The program aims to print out a script
+ *
+ * The command printf was used insted of puts
+ */
+int main(void)
+{
+	printf("with proper grammar, the outcome is a piece of art,\n");
+	return (0);
+}
