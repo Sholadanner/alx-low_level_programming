bc3679bb6cd08572c20457a00f8ee1b7ef108952
Sholadanner <judedanner@gmail.com>
2023-09-09 08:07:02 -0700

    Printing single digits of base 10, using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the data type for the main function is int
+ *
+ * Return: the return argument is void, 0
+ *
+ * The program is aiming to print integers with the use of putchar
+ */
+
+int main(void)
+{
+	int du;
+	
+	for (du = 0; du <= 9; du++)
+	{
+		putchar ('0' + du);
+	}
+	putchar ('\n');
+	return (0);
+}
