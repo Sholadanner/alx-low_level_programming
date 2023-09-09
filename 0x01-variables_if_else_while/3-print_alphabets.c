2e75e9490e1582185d2180b80e0f1d56a77435a4
Sholadanner <judedanner@gmail.com>
2023-09-09 06:05:34 -0700

    Printing the letters of the alphabet in caps and no caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - the data type for this code is char
+ *
+ * Return: the return for this argument is void, 0
+ *
+ * This program aims to print the letters of the alphabet
+ * in both upper and lower case
+ */
+
+int main(void)
+{
+	char dd;
+
+	for (dd == 'a'; dd <= 'z'; dd++)
+	{
+		putchar(dd);
+	} for (dd == 'A'; dd <= 'Z'; dd++)
+	{
+		putchar(dd);
+	}
+	putchar('\n');
+	return (0);
+}
+
