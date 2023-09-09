edd08927a0dac296ea16f706c666ca2e4998001f
Sholadanner <judedanner@gmail.com>
2023-09-09 08:49:41 -0700

    Printing base 16 digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the funtion main has an int data type
+ *
+ * Return: the return argument is void, 0
+ *
+ * The aim of this code is to print all base 16 numbers
+ */
+
+int main(void)
+{
+	int ds;
+	char od;
+
+	for (ds = 0; ds <=10; ds++)
+	{
+		putchar ('0' + ds);
+	} for (od = 'a'; od <= 'f'; od++);
+	{
+		putchar (od);
+	}
+	putchar ('\n');
+	return (0);
+}
