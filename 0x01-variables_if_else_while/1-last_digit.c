a7ecf7bd68dae33fae97f8678277ea52f09692d7
Sholadanner <judedanner@gmail.com>
2023-09-08 19:03:40 -0700

    commit for task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,6 +24,9 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	} else {
 		printf("Last digit of %d is %d and is is less than 6 and not 0\n", n, m);
+
+	printf("\n");
+
 	return (0);
 	}
 }
