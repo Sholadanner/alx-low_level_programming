f661507da45d44b46154f1205f1809375365db3e
Sholadanner <judedanner@gmail.com>
2023-09-09 07:36:42 -0700

    Printing all the single digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function has a int data type
+ *
+ * Return: the return for the argument is void, 0
+ *
+ * This program aims to print out all single digit numbers
+ * in base 10.
+ */
+
+int main(void)
+{
+	int u;
+
+	for (u = 0; u <= 10; u++)
+	{
+		printf ("%d\n", u);
+	}
+	return (0);
