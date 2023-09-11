a2673a29363498138bddfb5dc3e9c01897a7fc4a
Sholadanner <judedanner@gmail.com>
2023-09-11 10:53:04 -0700

    Printing string to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <string.h>
+
 /**
  * main - this program shows the function with int return type
  *
@@ -6,11 +8,13 @@
  *
  * This program intends to print a string
  */
+
 int main(void)
 {
-	const char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *m =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, m, sizeof(m) - 1);
+	write(STDERR_FILENO, m, strlen(m));
 
 	return (1);
 }
