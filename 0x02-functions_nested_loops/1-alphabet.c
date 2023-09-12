cd37a377c98081234a65b2f26ec3927fbdd700aa
Sholadanner <judedanner@gmail.com>
2023-09-12 16:01:03 -0700

    Printing the letters of the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - the main function for this code is of type int
+ *
+ * Return: the return value is void
+ *
+ * This code aims to print the alphabet
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar (a);
+	}
+	_putchar ('\n');
+	return (0);
+}
