e5352b740d04999bbf3ec91740757753e4e21ba1
Sholadanner <judedanner@gmail.com>
2023-09-12 17:08:39 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,17 @@
  */
 
 void print_alphabet_x10(void)
+
 {
-	char i;
 	int n;
+	char i;
 
 	for (n = 0; n <= 9; n++)
 	{
-		for (i = 'a'; i = 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar (i);
 		}
 		_putchar ('\n');
 	}
-
 }
