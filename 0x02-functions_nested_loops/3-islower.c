4cc9fb4342846884396e0180457af4d526689b9f
Sholadanner <judedanner@gmail.com>
2023-09-12 17:19:14 -0700

    Uppercase or Lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_islower - the function is of type int
+ *
+ *Return: the return value is c
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a'; && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
