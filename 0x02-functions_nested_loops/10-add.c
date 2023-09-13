0d929041a21b9786fbfc4414e84b2e42f2224356
Sholadanner <judedanner@gmail.com>
2023-09-12 19:49:49 -0700

    adding numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * add - data type int
+ *
+ * Return: value computed
+ *
+ * @a: first number to be added
+ *
+ * @b: second number to be added
+ */
+
 int add(int a, int b)
 {
 	return (a + b);
