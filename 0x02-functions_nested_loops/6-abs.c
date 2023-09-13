2af84b4851268ff232aba75a65ea55b66ee1bc5e
Sholadanner <judedanner@gmail.com>
2023-09-12 18:19:53 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function has an int data type
+ *
+ * Return: return value is abs_val or c
+ *
+ * @c: to be checked
+ */
+
+int _abs(int)
+{
+	if (c < 0)
+	{
+		int _abs;
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
