2a06738145dafc93a211323b2ac2a7e0d5883112
Sholadanner <judedanner@gmail.com>
2023-09-13 16:28:07 -0700

    9 time tabke

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,33 +7,11 @@
 
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int x = 9;
+	int y;
 
-	for (a = 0; b <= 9; a++)
+	for (y = 0; y <= 12; y++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a + b;
-			if (c > 9)
-			{
-				d = c % 10;
-				e = (c - d) / 10;
-				_putchar (44);
-				_putchar (32);
-				_putchar (e + '0');
-				_putchar (d + '0');
-			}
-			else
-			{
-				if (b != 0)
-				{
-					_putchar (44);
-					_putchar (32);
-					_putchar (32);
-				}
-				_putchar (c + '0');
-			}
-		}
-		_putchar ('\n');
+		_putchar ((x*y) + 0);
 	}
 }
