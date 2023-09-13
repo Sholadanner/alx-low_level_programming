9e1ce31636aa10809f53b64da471ff68c0bfe53b
Sholadanner <judedanner@gmail.com>
2023-09-12 17:53:26 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * Print_sign - thi function is of type int
+ *
+ * Return: the return value will be either 0, 1, or -1
+ *
+ * @n: to be checked
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar (+);
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar (0 +'0');
+		return (0);
+	}
+	else
+	{
+		_putchar (-);
+		return (-1);
+	}
+}
