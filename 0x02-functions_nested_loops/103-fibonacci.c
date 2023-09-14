3456d56d148b1c1e239da3d1434f7065489b52fb
Sholadanner <judedanner@gmail.com>
2023-09-13 19:31:13 -0700

    commit 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	long j = 1;
+	long k = 2;
+	long sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		i++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
