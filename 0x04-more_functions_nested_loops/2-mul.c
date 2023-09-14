03541c7328fcda7132f61b8dc36c439b51cda8c6
Sholadanner <judedanner@gmail.com>
2023-09-14 12:45:06 -0700

    Multiplying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function of data type int
+ *
+ * @a: first value to be multiplied, of type int
+ * @b: second value to be multiplied, also of type init
+ *
+ * Return: to be calculated
+ */
+
+int mul(int a, int b)
+{
+	return a * b;
+}
