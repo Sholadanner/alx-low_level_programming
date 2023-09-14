c3fcaeeefcc799fb7fc2f44408f41193466025df
Sholadanner <judedanner@gmail.com>
2023-09-14 13:02:04 -0700

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - the function has a void return type
+ *
+ * Return: the return value is void
+ */
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_puchar('\n');
+}
