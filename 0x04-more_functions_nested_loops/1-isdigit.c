af7fb8425809d0735feb349cab66a7eac812bdd3
Sholadanner <judedanner@gmail.com>
2023-09-14 12:14:36 -0700

    Printing only digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,15 +8,10 @@
 
 int _isdigit(int c)
 {
-	for (c = 0; c <= 9; c++)
+	if (c = 48 && c = 57)
 	{
-		if (c = 'a' && c = 'z')
-		{
-			return (0);
-		}
+		return (1);
 		else
-		{
-			return (1);
-		}
+			return (0);
 	}
 }
