b31aa7c3fe88d9a67af5f577c1835fe857bc0b67
Sholadanner <judedanner@gmail.com>
2023-09-13 19:17:05 -0700

    commit 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 0;
+	long y = 1;
+	long z = 2;
+
+	while (x < 50)
+	{
+		if (x == 0)
+
+		printf("%ld", y);
+		else if (x == 1)
+			printf(", %ld", z);
+		else
+		{
+			z += y;
+			y = z - y;
+			printf(", %ld", z);
+		}
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
