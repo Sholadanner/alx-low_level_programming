b32cb1c742469fda98a22251bdc5c8867aa8906c
Sholadanner <judedanner@gmail.com>
2023-09-14 12:06:26 -0700

    Printing only digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.j"
+
+/**
+ * _isdigit - has an int return type
+ *
+ * Return: the return value is to be calculated
+ */
+
+int _isdigit(int c)
+{
+	for (c = 0; c <= 9; c++)
+	{
+		if (c = 'a' && c = 'z')
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+}
