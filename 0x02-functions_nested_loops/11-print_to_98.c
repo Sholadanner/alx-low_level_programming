5061f2f7612fad5523ad91d412a2cb13d961ee56
Sholadanner <judedanner@gmail.com>
2023-09-13 17:50:17 -0700

    Printing natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,14 +17,14 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf ("%d", n);
+				printf("%d", n);
 
-				printf ("\n");
+				printf("\n");
 				break;
 			}
 			else
 			{
-				printf ("%d, ", n);
+				printf("%d, ", n);
 			}
 		}
 	}
@@ -34,13 +34,13 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf ("%d", n);
-				printf ("\n");
+				printf("%d", n);
+				printf("\n");
 				break;
 			}
 			else
 			{
-				printf ("%d, ", n);
+				printf("%d, ", n);
 			}
 		}
 	}
