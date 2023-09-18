57fd335973cc847d3721c9c2b870c9ee7063eae5
Sholadanner <judedanner@gmail.com>
2023-09-18 14:11:16 -0700

    Counting lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - countig lenght of string
+ *
+ * Return: c
+ *
+ * @s - string to be counted
+ */
+
+int _strlen(char *s)
+{
+	int a;
+	int c;
+
+	for (a = 0; s[i] != '\0'; a++)
+		c++;
+	return (c);
+}
