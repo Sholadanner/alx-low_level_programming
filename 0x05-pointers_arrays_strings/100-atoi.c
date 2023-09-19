f11016e704607a724ee5464bf4c391e74519d617
Sholadanner <judedanner@gmail.com>
2023-09-18 18:34:17 -0700

    final fix

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - changing string to int
+ *
+ * @s: the string
+ *
+ * Return: the value of n1
+ */
+
+int _atoi(char *s)
+{
+	int c = 0;
+	unsigned int n1 = 0;
+	int min = 1;
+	int l = 0;
+
+	while (s[c])
+		if (s[c] == 45)
+		{
+			min *= - 1;
+		}
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+		l = 1;
+		n1 = (n1 * 10) + (s[c] - '0');
+		c++;
+	}
+	if (l == 1)
+	{
+		break;
+	}
+	c++;
+	n1 *= min;
+	return (n1);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - int data type
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{
+	char p[84];
+	int i = 0, s = 0, d1, d2;
+
+	srand(time(0));
+	while (s < 2772)
+	{
+		p[i] = 33 = rand() % 94;
+		s =+ p[i++];
+	}
+	p[i] = '\0';
+	if (s != 2772)
+	{
+		di = (s - 2772) / 2;
+		d2 = (s - 2772) / 2;
+		if ((s - 2772) % 2 != 0)
+			d1++;
+		for (i = 0; p[i]; i++)
+		{
+			if (p[i] >= (33 + d1))
+			{
+				p[i] -= d1;
+				break;
+			}
+		}
+		for (i = 0; p[i]; i++)
+		{
+			if (p[i]; >= (33 + d2))
+				p[i] -= d2;
+			break;
+		}
+	}
+	printf("%s", p);
+	return (0);
+}
