a2604f44afc35c8076d4ef62900d544e9bce6f5d
Sholadanner <judedanner@gmail.com>
2023-09-18 17:32:39 -0700

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,10 +14,10 @@ void rev_string(char *s)
 	while (s[i++])
 		l++;
 
-	for (i = l - 1; i >= l /2; i--)
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		t = s[i];
 		s[i] = s[l - i - 1];
-		s[l - i -1] = t;
+		s[l - i - 1] = t;
 	}
 }
