2bffb954bfba7a5f0e8efae5533494b3b156a3af
Sholadanner <judedanner@gmail.com>
2023-09-19 19:26:06 -0700

    New changes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,27 +10,30 @@
 
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int n1 = 0;
-	int min = 1;
-	int l = 0;
+int i, value, sign;
+char c;
 
-	while (s[c])
-		if (s[c] == 45)
-		{
-			min *= - 1;
-		}
-	while (s[c] >= 48 && s[c] <= 57)
-	{
-		l = 1;
-		n1 = (n1 * 10) + (s[c] - '0');
-		c++;
-	}
-	if (l == 1)
-	{
-		break;
-	}
-	c++;
-	n1 *= min;
-	return (n1);
+i = 0;
+value = 0;
+sign = -1;
+while (s[i] != '\0')
+{
+c = s[i];
+if (c == '-')
+sign *= -1;
+else if (c >= '0' && c <= '9')
+{
+if (value < 0)
+value = value * 10 - (c - '0');
+else
+value = (c - '0') * -1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+}
+i++;
+}
+if (sign < 0)
+value *= -1;
+
+return (0);
 }
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,37 +10,18 @@
 
 int main(void)
 {
-	char p[84];
-	int i = 0, s = 0, d1, d2;
+	int myrand;
+	int count;
+	int total;
 
-	srand(time(0));
-	while (s < 2772)
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
 	{
-		p[i] = 33 + rand() % 94;
-		s =+ p[i++];
+	myrand = (rand() % 125) + 1;
+	printf("%c", myrand);
+	total -= myrand;
 	}
-	p[i] = '\0';
-	if (s != 2772)
-	{
-		d1 = (s - 2772) / 2;
-		d2 = (s - 2772) / 2;
-		if ((s - 2772) % 2 != 0)
-			d1++;
-		for (i = 0; p[i]; i++)
-		{
-			if (p[i] >= (33 + d1))
-			{
-				p[i] -= d1;
-				break;
-			}
-		}
-		for (i = 0; p[i]; i++)
-		{
-			if (p[i] >= (33 + d2))
-				p[i] -= d2;
-			break;
-		}
-	}
-	printf("%s", p);
+	printf("%c", total);
+
 	return (0);
 }
