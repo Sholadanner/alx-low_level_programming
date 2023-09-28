1473eaaeeda3b9b2e0b7129ae58af782317b0f4e
Sholadanner <judedanner@gmail.com>
2023-09-28 13:46:50 -0700

    printing string in revrse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - printing string in revrse
+ *
+ * @s: string to be printed in reverse
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		return;
+	}
+}
