7c67dec1baf1acfc93e40f28e65962941083fd7a
Sholadanner <judedanner@gmail.com>
2023-10-02 19:34:22 -0700

    commit 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function to be executed
+ *
+ * @argc: argument count
+ * @argv: agrument vector
+ * Return: 0
+ */
+
+int main(int agrc, char *argv[])
+{
+	(void) agrc;
+
+	printf("%s\n", agrv[0]);
+	return (0);
+}
