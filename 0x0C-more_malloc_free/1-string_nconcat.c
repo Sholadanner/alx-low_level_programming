ccddfced7e51e06e19597ba85a9933c798def3c0
Sholadanner <judedanner@gmail.com>
2023-10-07 12:51:37 -0700

    hot fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,57 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * string_nconcat - function that concatenates two strings
- *
- * @s1: first string to be concatenated
- *
- * @s2: second string to be concatenated
- *
- * @n: number of bytes of the second string should be copied
- *
- * Return: a pointer to the allocated memory for the
- * concatenation if successful, if unsuccessful, the
- * return value will be NULL and if NULL is passed to
- * the parameters an empty string will be returned
- */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int slen1 = 0;
-	unsigned int slen2 = 0;
+	unsigned int j;
+	unsigned int i;
+	unsigned int strlen1 = 0;
+	unsigned int strlen2 = 0;
 	char *cat;
 
-	if (s1 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return ("");
-	if (s2 == NULL)
-		return ("");
-	while (s1[i])
-		i++;
-	while (s2[slen2])
-		slen2++;
-	if (n >= slen2)
-		slen1 = i + slen2;
-	else
-		slen1 = i + n;
-
-	cat = malloc(sizeof(char) * (slen1 + 1));
+	for (i = 0; s1[i] != '\0'; i++)
+		strlen1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		strlen2++;
+	if (n >= strlen2)
+		n = strlen2;
+	
+	cat = malloc(sizeof(char) * (strlen1 + n + 1));
+	for (i = 0; i < strlen1; i++)
+		cat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		cat[i + j] = s2[j];
 	if (cat == NULL)
 		return (NULL);
-	slen2 = 0;
-	while (j < slen1)
-	{
-		if (j <= i)
-			cat[j] = s1[j];
-		if (j >= i)
-		{
-			cat[j] = s2[slen2];
-			slen2++;
-		}
-		j++;
-	}
-	cat[j] = '\0';
+	cat[i + j] = '\0';
 	return (cat);
 }
