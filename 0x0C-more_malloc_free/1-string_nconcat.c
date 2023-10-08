c961aba6d9a2e86d282610db0f955966563fc60a
Sholadanner <judedanner@gmail.com>
2023-10-08 16:33:17 -0700

    final try

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,35 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenats two strings
- * @s1: first string to be concatenated
+ * string_nconcat - function that concatenates two strings
+ * @s1: the first string to be concatenated
  * @s2: second string to be concatenated
- * @n: the  number of bytes of the second string that should
- * be concatenated
- * Return: the be determined
+ * @n: amount of bytes to be concatenated
+ * Return: to be determined
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	int i;
 	unsigned int j;
-	unsigned int i;
-	unsigned int strlen1 = 0;
-	unsigned int strlen2 = 0;
+	int strlen1 = 0;
+	int strlen2 = 0;
 	char *cat;
 
-	if (s1 == NULL)
-		return ("");
-	if (s2 == NULL)
-		return ("");
+	if (s1 == NULL || s2 == NULL)
+		return ('\0');
 	for (i = 0; s1[i] != '\0'; i++)
 		strlen1++;
 	for (i = 0; s2[i] != '\0'; i++)
 		strlen2++;
-
-	cat = malloc(sizeof(char) * (strlen1 + n + 1));
+	cat = malloc(sizeof(char) * (strlen1 + n) + 1);
 	for (i = 0; i < strlen1; i++)
 		cat[i] = s1[i];
 	for (j = 0; j < n && s2[j] != '\0'; j++)
 		cat[strlen1 + j] = s2[j];
+	cat[strlen1 + j] = '\0';
 	if (cat == NULL)
 		return (NULL);
-	cat[strlen1 + j] = '\0';
 	return (cat);
 }
