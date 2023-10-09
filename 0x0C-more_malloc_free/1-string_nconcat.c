808640409cecab7e1ca292fb4f498f86ae1f4b7a
Sholadanner <judedanner@gmail.com>
2023-10-09 04:03:49 -0700

    fixes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,6 +23,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		strlen1++;
 	for (i = 0; s2[i] != '\0'; i++)
 		strlen2++;
+	if (n > strlen2)
+		n = strlen2;
 	cat = malloc(sizeof(char) * (strlen1 + n) + 1);
 	if (cat == NULL)
 		return (NULL);
