38ee3c8544def2566db5be6d710b006f1821edd4
Sholadanner <judedanner@gmail.com>
2023-10-11 18:22:33 -0700

    finding an interger

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - fuction that searches for an interger
+ * @array: the array the element to be examined
+ * @size: the size of the array
+ * @cmp: pointer to a function that compares elements
+ * inside the array
+ * Return: to be determined
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return -1;
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
